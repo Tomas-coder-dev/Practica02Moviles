d7c1ce7b4c7537f3457f34d71313307b5ae13c4b
Tomas-coder-dev <tomasidk2004@gmail.com>
2024-10-20 15:51:36 -0500

    ultima actualizacion

--- app/src/main/java/com/aylas/moreno/usolayoutsv4/DetallesPedidoActivity.kt
+++ app/src/main/java/com/aylas/moreno/usolayoutsv4/DetallesPedidoActivity.kt
@@ -3,6 +3,7 @@ package com.aylas.moreno.usolayoutsv4
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.aylas.moreno.usolayoutsv4.databinding.ActivityDetallesPedidoBinding
 
@@ -39,20 +40,28 @@ class DetallesPedidoActivity : AppCompatActivity() {
         // Botón de WhatsApp
         binding.btnWhatsapp.setOnClickListener {
             val mensaje = "Hola $nombreCliente, tus productos: $productos están en camino a la dirección: $direccion."
-            val uri = Uri.parse("smsto:$numeroCliente")
-            val intent = Intent(Intent.ACTION_SENDTO, uri).apply {
-                setPackage("com.whatsapp")
-                putExtra(Intent.EXTRA_TEXT, mensaje)
+            try {
+                val uri = Uri.parse("smsto:$numeroCliente")
+                val intent = Intent(Intent.ACTION_SENDTO, uri).apply {
+                    setPackage("com.whatsapp")
+                    putExtra(Intent.EXTRA_TEXT, mensaje)
+                }
+                startActivity(intent)
+            } catch (e: Exception) {
+                Toast.makeText(this, "WhatsApp no está instalado", Toast.LENGTH_SHORT).show()
             }
-            startActivity(intent)
         }
 
         // Botón de Google Maps
         binding.btnMaps.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW).apply {
-                data = Uri.parse("geo:0,0?q=$direccion, $ciudad")
+            try {
+                val uri = Uri.parse("geo:0,0?q=$direccion, $ciudad")
+                val intent = Intent(Intent.ACTION_VIEW, uri)
+                intent.setPackage("com.google.android.apps.maps")
+                startActivity(intent)
+            } catch (e: Exception) {
+                Toast.makeText(this, "Google Maps no está instalado", Toast.LENGTH_SHORT).show()
             }
-            startActivity(intent)
         }
     }
 }
